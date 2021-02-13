21d611f6f25d6a4c438fae01a88449b3e9a12208
ryan <ryanhcode.dev@gmail.com>
2021-02-12 23:51:18 -0500

    Stat storage

--- src/main/java/com/ryanhcode/hotchicks/HotChicks.java
+++
@@ -1,7 +0,0 @@
-package com.ryanhcode.hotchicks;
-
-public class HotChicks {
-    public static final String MODID = "hotchicks";
-    public static final String NAME = "Hot Chicks";
-    public static final String VERSION = "0.0";
-}
