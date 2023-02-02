b28c7da2a2047142be5b852667c29b2c60f9b2c4
Mnesikos <mnesikos@gmail.com>
2023-02-02 12:59:06 -0500

    add 2 new crops to farmer trades

--- src/main/java/com/github/kmfisk/hotchicks/entity/merchant/villager/HotVillagerTrades.java
+++ src/main/java/com/github/kmfisk/hotchicks/entity/merchant/villager/HotVillagerTrades.java
@@ -8,10 +8,12 @@ import net.minecraftforge.event.village.VillagerTradesEvent;
 public class HotVillagerTrades {
     public static void onVillagerTradesEvent(VillagerTradesEvent event) {
         if (event.getType() == VillagerProfession.FARMER) {
-            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.LETTUCE.get(), 1, 4, 16, 1));
-            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.GARLIC.get(), 1, 4, 16, 1));
-            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.OKRA.get(), 1, 4, 16, 1));
+            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.CABBAGE.get(), 1, 4, 16, 1));
+            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.CUCUMBER.get(), 1, 4, 16, 1));
             event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.COTTON.get(), 1, 4, 16, 1));
+            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.GARLIC.get(), 1, 4, 16, 1));
+            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.LETTUCE.get(), 1, 4, 16, 1));
+            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeraldsTrade(HotItems.OKRA.get(), 1, 4, 16, 1));
         }
     }
 }
