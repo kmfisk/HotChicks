7ce2354e0438754ff326b443114b2ba5262041c1
Mnesikos <mnesikos@gmail.com>
2023-02-07 00:36:58 -0500

    add some smelting & cooking data gen

--- src/main/java/com/github/kmfisk/hotchicks/data/HotRecipeProvider.java
+++ src/main/java/com/github/kmfisk/hotchicks/data/HotRecipeProvider.java
@@ -2,15 +2,20 @@ package com.github.kmfisk.hotchicks.data;
 
 import com.github.kmfisk.hotchicks.HotChicks;
 import com.github.kmfisk.hotchicks.block.HotBlocks;
+import com.github.kmfisk.hotchicks.item.HotItems;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonObject;
 import net.minecraft.block.Blocks;
 import net.minecraft.data.*;
 import net.minecraft.item.Items;
+import net.minecraft.item.crafting.CookingRecipeSerializer;
+import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.IItemProvider;
+import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -36,6 +41,19 @@ public class HotRecipeProvider extends RecipeProvider {
                         Ingredient.of(Items.APPLE),
                         Ingredient.of(Items.APPLE)
                 )
+        ));
+
+        consumer.accept(smeltingRecipeResult(HotItems.COOKED_BEEF_STEAK.get(),
+                Ingredient.of(HotItems.BEEF_STEAK.get()),
+                0.35F, 200
+        ));
+        consumer.accept(cookingRecipeResult("smoking", HotItems.COOKED_BEEF_STEAK.get(),
+                Ingredient.of(HotItems.BEEF_STEAK.get()),
+                0.35F, 100, IRecipeSerializer.SMOKING_RECIPE
+        ));
+        consumer.accept(cookingRecipeResult("campfire", HotItems.COOKED_BEEF_STEAK.get(),
+                Ingredient.of(HotItems.BEEF_STEAK.get()),
+                0.35F, 600, IRecipeSerializer.CAMPFIRE_COOKING_RECIPE
         ));
     }
 
@@ -66,6 +84,44 @@ public class HotRecipeProvider extends RecipeProvider {
                 recipe,
                 null,
                 null
+        ) {
+            @Override
+            public JsonObject serializeAdvancement() {
+                return null;
+            }
+        };
+    }
+
+    public static CookingRecipeBuilder.Result smeltingRecipeResult(IItemProvider iItemProvider, Ingredient recipe, float experience, int cookingTime) {
+        return new CookingRecipeBuilder.Result(
+                iItemProvider.asItem().getRegistryName(),
+                HotChicks.MOD_ID,
+                recipe,
+                iItemProvider.asItem(),
+                experience,
+                cookingTime,
+                null,
+                null,
+                IRecipeSerializer.SMELTING_RECIPE
+        ) {
+            @Override
+            public JsonObject serializeAdvancement() {
+                return null;
+            }
+        };
+    }
+
+    public static CookingRecipeBuilder.Result cookingRecipeResult(String cookingMethod, IItemProvider iItemProvider, Ingredient recipe, float experience, int cookingTime, CookingRecipeSerializer<?> cookingSerializer) {
+        return new CookingRecipeBuilder.Result(
+                new ResourceLocation(HotChicks.MOD_ID, iItemProvider.asItem().getRegistryName().getPath() + "_" + cookingMethod),
+                HotChicks.MOD_ID,
+                recipe,
+                iItemProvider.asItem(),
+                experience,
+                cookingTime,
+                null,
+                null,
+                cookingSerializer
         ) {
             @Override
             public JsonObject serializeAdvancement() {
