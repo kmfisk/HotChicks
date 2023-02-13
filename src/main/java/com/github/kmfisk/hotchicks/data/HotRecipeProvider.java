80c45b22bf0b27ba910bcdc77675885af3f1aa8e
Mnesikos <mnesikos@gmail.com>
2023-02-13 18:52:06 -0500

    add missing recipes & butter block

--- src/main/java/com/github/kmfisk/hotchicks/data/HotRecipeProvider.java
+++ src/main/java/com/github/kmfisk/hotchicks/data/HotRecipeProvider.java
@@ -3,6 +3,7 @@ package com.github.kmfisk.hotchicks.data;
 import com.github.kmfisk.hotchicks.HotChicks;
 import com.github.kmfisk.hotchicks.block.HotBlocks;
 import com.github.kmfisk.hotchicks.item.HotItems;
+import com.github.kmfisk.hotchicks.tags.HotItemTags;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonObject;
@@ -15,6 +16,7 @@ import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 import java.util.List;
 import java.util.Map;
@@ -27,6 +29,14 @@ public class HotRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildShapelessRecipes(Consumer<IFinishedRecipe> consumer) {
+        consumer.accept(shapedRecipeResult(HotBlocks.FOOD_CROCK.get(), 2, ImmutableList.of("FCF", " F "),
+                ImmutableMap.<Character, Ingredient>builder().put('F', Ingredient.of(Items.CLAY_BALL)).put('C', Ingredient.of(Items.BOWL)).build()));
+        consumer.accept(shapedRecipeResult(HotBlocks.HUTCH_GATE.get(), 2, ImmutableList.of("SCS", "SFS", "SCS"),
+                ImmutableMap.<Character, Ingredient>builder().put('F', Ingredient.of(Items.IRON_NUGGET)).put('S', Ingredient.of(Items.STICK)).put('C', Ingredient.of(Items.IRON_INGOT)).build()));
+        consumer.accept(shapedRecipeResult(HotBlocks.HUTCH_FLOOR.get(), 4, ImmutableList.of("FFF", "FCF", "FFF"),
+                ImmutableMap.<Character, Ingredient>builder().put('F', Ingredient.of(Items.STICK)).put('C', Ingredient.of(HotBlocks.HUTCH_BARS.get())).build()));
+        consumer.accept(shapedRecipeResult(HotBlocks.HUTCH_BARS.get(), 16, ImmutableList.of("FCF", "FCF", "FCF"),
+                ImmutableMap.<Character, Ingredient>builder().put('F', Ingredient.of(Items.IRON_NUGGET)).put('C', Ingredient.of(Items.IRON_INGOT)).build()));
         consumer.accept(shapedRecipeResult(HotItems.LIVESTOCK_CRATE.get(), 1, ImmutableList.of("TTT", "PCP", "PPP"),
                 ImmutableMap.<Character, Ingredient>builder().put('T', Ingredient.of(ItemTags.WOODEN_TRAPDOORS)).put('C', Ingredient.of(Blocks.CHEST)).put('P', Ingredient.of(ItemTags.PLANKS)).build()));
         consumer.accept(shapedRecipeResult(HotBlocks.NEST.get(), 2, ImmutableList.of("S S", "SFS"),
@@ -137,6 +147,87 @@ public class HotRecipeProvider extends RecipeProvider {
                 ImmutableList.of(Ingredient.of(HotBlocks.CITRON_SAPLING.get()), Ingredient.of(HotBlocks.POMELO_SAPLING.get()))));
         consumer.accept(namedShapelessRecipeResult("yuzu_hybrid_seed", HotBlocks.YUZU_SAPLING.get(), 2,
                 ImmutableList.of(Ingredient.of(HotBlocks.PAPEDA_SAPLING.get()), Ingredient.of(HotBlocks.MANDARIN_SAPLING.get()))));
+
+        consumer.accept(smeltingRecipeResult(HotBlocks.BUTTER_BLOCK.get(), Ingredient.of(Items.MILK_BUCKET), 0.35F, 200));
+        consumer.accept(shapelessRecipeResult(HotItems.BUTTER.get(), 4, ImmutableList.of(Ingredient.of(HotBlocks.BUTTER_BLOCK.get()))));
+        consumer.accept(shapedRecipeResult(HotItems.PLANT_MILK.get(), 4, ImmutableList.of("PPP", "PWP", "PPP"),
+                ImmutableMap.<Character, Ingredient>builder().put('P', Ingredient.of(HotItemTags.PLANT_MILK_INGREDIENTS)).put('W', Ingredient.of(Items.WATER_BUCKET)).build()));
+        consumer.accept(shapelessRecipeResult(HotItems.CITRUS_COOLER.get(), 2,
+                ImmutableList.of(Ingredient.of(HotItemTags.CITRUS_FRUITS), Ingredient.of(HotItemTags.CITRUS_FRUITS), Ingredient.of(HotItemTags.CITRUS_FRUITS), Ingredient.of(Items.SUGAR), Ingredient.of(Blocks.ICE))));
+        consumer.accept(shapelessRecipeResult(HotItems.BERRY_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.STRAWBERRY.get()), Ingredient.of(HotItems.BLUEBERRIES.get()), Ingredient.of(Items.SWEET_BERRIES))));
+        consumer.accept(shapelessRecipeResult(HotItems.CHOCOLATE_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(Items.COCOA_BEANS), Ingredient.of(Items.COCOA_BEANS), Ingredient.of(HotItems.BANANA.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.CREAMSICLE_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.ORANGE.get()), Ingredient.of(HotItems.GRAPEFRUIT.get()), Ingredient.of(HotItems.MANDARIN.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.KEY_LIME_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.LEMON.get()), Ingredient.of(HotItems.LIME.get()), Ingredient.of(HotItems.CITRON.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.PEACH_MANGO_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.LEMON.get()), Ingredient.of(HotItems.PEACH.get()), Ingredient.of(HotItems.MANGO.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.SUPERFOOD_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.BLUEBERRIES.get()), Ingredient.of(HotItems.KALE.get()), Ingredient.of(HotItems.OATS.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.TROPICAL_PROTEIN_SHAKE.get(), 2,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.BANANA.get()), Ingredient.of(HotItems.MANGO.get()), Ingredient.of(HotItems.KIWI.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.SMOOTHIE.get(), 2,
+                ImmutableList.of(Ingredient.of(HotItemTags.FRUITS), Ingredient.of(HotItemTags.FRUITS), Ingredient.of(HotItemTags.FRUITS), Ingredient.of(Items.SUGAR), Ingredient.of(Blocks.SNOW_BLOCK))));
+        consumer.accept(shapelessRecipeResult(HotItems.CABBAGE_ROLLS.get(), 1,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_PORKCHOPS), Ingredient.of(HotItems.RICE.get()), Ingredient.of(HotItems.RICE.get()), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.CABBAGE.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.CHEESE_PLATE.get(), 2,
+                ImmutableList.of(Ingredient.of(HotItems.HARD_CHEESE.get()), Ingredient.of(HotItems.SOFT_CHEESE.get()), Ingredient.of(HotItems.GRAPES.get()), Ingredient.of(HotItems.POMEGRANATE.get()), Ingredient.of(HotItems.FIG.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.FARMERS_BREAKFAST.get(), 1,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_PORKCHOPS), Ingredient.of(Items.BREAD), Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItemTags.FRUITS))));
+        consumer.accept(shapelessRecipeResult(HotItems.FRUIT_BOWL.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItemTags.FRUIT_SALAD_INGREDIENTS), Ingredient.of(HotItemTags.FRUIT_SALAD_INGREDIENTS), Ingredient.of(HotItemTags.FRUIT_SALAD_INGREDIENTS), Ingredient.of(HotItemTags.FRUIT_SALAD_INGREDIENTS))));
+        consumer.accept(shapelessRecipeResult(HotItems.GLAZED_PORK_CHOP.get(), 1,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_PORKCHOPS), Ingredient.of(Items.HONEY_BOTTLE), Ingredient.of(HotItems.PEACH.get()), Ingredient.of(Items.POTATO), Ingredient.of(HotItems.PEPPERS.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.KIMCHI.get(), 3,
+                ImmutableList.of(Ingredient.of(HotItems.CABBAGE.get()), Ingredient.of(HotItems.CABBAGE.get()), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItems.GARLIC.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.LETTUCE_WRAP.get(), 2,
+                ImmutableList.of(Ingredient.of(HotItemTags.COOKED_MEATS), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(HotItems.LETTUCE.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.OATMEAL.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItemTags.FRUITS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.OATS.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.PAVLOVA.get(), 4,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItems.STRAWBERRY.get()), Ingredient.of(HotItems.KIWI.get()), Ingredient.of(Items.SUGAR), Ingredient.of(HotItemTags.MILKS))));
+        consumer.accept(shapelessRecipeResult(HotItems.PICKLES.get(), 3,
+                ImmutableList.of(Ingredient.of(HotItems.CUCUMBER.get()), Ingredient.of(HotItems.CUCUMBER.get()), Ingredient.of(Tags.Items.SEEDS), Ingredient.of(HotItems.GARLIC.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.PORK_CUTLET.get(), 1,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_PORKCHOPS), Ingredient.of(Items.WHEAT), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItems.RICE.get()), Ingredient.of(HotItems.CABBAGE.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.RABBIT_TACOS.get(), 2,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_RABBITS), Ingredient.of(HotItems.CORN.get()), Ingredient.of(HotItems.CORN.get()), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.LIME.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.CHICKEN_SALAD.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(HotItemTags.COOKED_CHICKENS), Ingredient.of(Items.APPLE), Ingredient.of(HotItems.GRAPES.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.CHINESE_CHICKEN_SALAD.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItemTags.COOKED_CHICKENS), Ingredient.of(HotItems.CABBAGE.get()), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.MANDARIN.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.DANDELION_SALAD.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(Items.DANDELION), Ingredient.of(HotItems.GARLIC.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.GARDEN_SALAD.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(HotItems.CORN.get()), Ingredient.of(HotItems.TOMATO.get()), Ingredient.of(HotItems.PEPPERS.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.POWER_SALAD.get(), 1,
+                ImmutableList.of(Ingredient.of(Items.BOWL), Ingredient.of(HotItems.LETTUCE.get()), Ingredient.of(HotItems.KALE.get()), Ingredient.of(HotItems.CABBAGE.get()), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(Items.EGG))));
+        consumer.accept(shapelessRecipeResult(HotItems.SALMON_RICE_BOWL.get(), 1,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_SALMONS), Ingredient.of(HotItems.RICE.get()), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.YUZU.get()), Ingredient.of(Items.KELP))));
+        consumer.accept(shapelessRecipeResult(HotItems.SHAKSHUKA.get(), 1,
+                ImmutableList.of(Ingredient.of(Tags.Items.EGGS), Ingredient.of(Tags.Items.EGGS), Ingredient.of(HotItems.TOMATO.get()), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItems.GARLIC.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.SHORT_RIB_BBQ.get(), 1,
+                ImmutableList.of(Ingredient.of(HotItemTags.RAW_BEEFS), Ingredient.of(Items.HONEY_BOTTLE), Ingredient.of(Items.APPLE), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItems.RICE.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.BEEF_CHILI_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(HotItemTags.RAW_BEEFS), Ingredient.of(HotItemTags.RAW_BEEFS), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItems.TOMATO.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.BEEF_STROGANOFF_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(HotItemTags.RAW_BEEFS), Ingredient.of(HotItemTags.RAW_BEEFS), Ingredient.of(HotItemTags.MILKS), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItems.BUTTER.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.BRAISED_RABBIT_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(HotItemTags.RAW_RABBITS), Ingredient.of(Items.CARROT), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItemTags.MILKS))));
+        consumer.accept(shapelessRecipeResult(HotItems.CHICKEN_NOODLE_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(HotItemTags.RAW_CHICKENS), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.PEAS.get()), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(Items.WHEAT))));
+        consumer.accept(shapelessRecipeResult(HotItems.GUMBO_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(HotItemTags.COOKED_CHICKENS), Ingredient.of(HotItemTags.COOKED_PORKCHOPS), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItems.PEPPERS.get()), Ingredient.of(HotItems.OKRA.get()), Ingredient.of(HotItems.RICE.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.POTATO_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(Items.POTATO), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItemTags.MILKS), Ingredient.of(HotItems.SOFT_CHEESE.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.SPLIT_PEA_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(Items.CARROT), Ingredient.of(HotItems.PEAS.get()), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItemTags.COOKED_PORKCHOPS))));
+        consumer.accept(shapelessRecipeResult(HotItems.VEGGIE_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(Items.CARROT), Ingredient.of(Items.POTATO), Ingredient.of(HotItems.GARLIC.get()), Ingredient.of(HotItems.PEAS.get()), Ingredient.of(HotItems.TOMATO.get()), Ingredient.of(HotItems.CORN.get()))));
+        consumer.accept(shapelessRecipeResult(HotItems.WILD_MUSHROOM_SOUP.get(), 4,
+                ImmutableList.of(Ingredient.of(Items.BUCKET), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(Tags.Items.MUSHROOMS), Ingredient.of(Items.CARROT), Ingredient.of(Items.POTATO), Ingredient.of(HotItems.GARLIC.get()))));
     }
 
     private static void cookRecipes(Consumer<IFinishedRecipe> consumer, IItemProvider iItemProvider, Ingredient recipe, float experience) {
---
+++ src/main/java/com/github/kmfisk/hotchicks/tags/HotItemTags.java
@@ -0,0 +1,25 @@
+package com.github.kmfisk.hotchicks.tags;
+
+import com.github.kmfisk.hotchicks.HotChicks;
+import net.minecraft.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
+
+public class HotItemTags {
+    public static final Tags.IOptionalNamedTag<Item> FRUIT_SALAD_INGREDIENTS = ItemTags.createOptional(new ResourceLocation(HotChicks.MOD_ID, "fruit_salad_ingredients"));
+    public static final Tags.IOptionalNamedTag<Item> PLANT_MILK_INGREDIENTS = ItemTags.createOptional(new ResourceLocation(HotChicks.MOD_ID, "plant_milk_ingredients"));
+
+    public static final Tags.IOptionalNamedTag<Item> CITRUS_FRUITS = ItemTags.createOptional(new ResourceLocation("forge", "fruits/citrus"));
+    public static final Tags.IOptionalNamedTag<Item> FRUITS = ItemTags.createOptional(new ResourceLocation("forge", "fruits"));
+    public static final Tags.IOptionalNamedTag<Item> MILKS = ItemTags.createOptional(new ResourceLocation("forge", "milks"));
+
+    public static final Tags.IOptionalNamedTag<Item> COOKED_MEATS = ItemTags.createOptional(new ResourceLocation("forge", "cooked_meats"));
+    public static final Tags.IOptionalNamedTag<Item> COOKED_CHICKENS = ItemTags.createOptional(new ResourceLocation("forge", "cooked_meats/cooked_chicken"));
+    public static final Tags.IOptionalNamedTag<Item> COOKED_PORKCHOPS = ItemTags.createOptional(new ResourceLocation("forge", "cooked_meats/cooked_porkchop"));
+    public static final Tags.IOptionalNamedTag<Item> RAW_BEEFS = ItemTags.createOptional(new ResourceLocation("forge", "raw_meats/beef"));
+    public static final Tags.IOptionalNamedTag<Item> RAW_CHICKENS = ItemTags.createOptional(new ResourceLocation("forge", "raw_meats/chicken"));
+    public static final Tags.IOptionalNamedTag<Item> RAW_PORKCHOPS = ItemTags.createOptional(new ResourceLocation("forge", "raw_meats/porkchop"));
+    public static final Tags.IOptionalNamedTag<Item> RAW_RABBITS = ItemTags.createOptional(new ResourceLocation("forge", "raw_meats/rabbit"));
+    public static final Tags.IOptionalNamedTag<Item> RAW_SALMONS = ItemTags.createOptional(new ResourceLocation("forge", "raw_fishes/salmon"));
+}
