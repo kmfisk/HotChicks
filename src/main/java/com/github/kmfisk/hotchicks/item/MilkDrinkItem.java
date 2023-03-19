a50fb5f5999006d747be508f917ff7c1ca6a04c4
Mnesikos <mnesikos@gmail.com>
2023-03-19 15:42:23 -0400

    milk actually removes effects

--- src/main/java/com/github/kmfisk/hotchicks/item/MilkDrinkItem.java
+++ src/main/java/com/github/kmfisk/hotchicks/item/MilkDrinkItem.java
@@ -2,7 +2,10 @@ package com.github.kmfisk.hotchicks.item;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.world.World;
+
+import java.util.Iterator;
 
 public class MilkDrinkItem extends DrinkItem {
     public MilkDrinkItem(Properties properties) {
@@ -11,7 +14,7 @@ public class MilkDrinkItem extends DrinkItem {
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, World level, LivingEntity livingEntity) {
-        if (!level.isClientSide) livingEntity.curePotionEffects(stack);
+        if (!level.isClientSide) livingEntity.removeAllEffects();
         return super.finishUsingItem(stack, level, livingEntity);
     }
 }
