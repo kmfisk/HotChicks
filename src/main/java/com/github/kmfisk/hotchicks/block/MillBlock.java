0611b0c9e69c9ff2132b2a6438cc7ba262ec43b5
Mnesikos <mnesikos@gmail.com>
2023-03-21 17:04:09 -0400

    add mill hitbox & rotations

--- src/main/java/com/github/kmfisk/hotchicks/block/MillBlock.java
+++ src/main/java/com/github/kmfisk/hotchicks/block/MillBlock.java
@@ -9,9 +9,12 @@ import net.minecraft.block.ContainerBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -22,14 +25,16 @@ import net.minecraft.world.World;
 import javax.annotation.Nullable;
 
 public class MillBlock extends ContainerBlock {
-    private static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 16.0D, 16.0D, 13.0D, 16.0D);
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    private static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 13.0D, 16.0D);
 
     public MillBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
-    public VoxelShape getShape(BlockState pState, IBlockReader pLevel, BlockPos pPos, ISelectionContext pContext) {
+    public VoxelShape getShape(BlockState state, IBlockReader level, BlockPos pos, ISelectionContext context) {
         return SHAPE;
     }
 
@@ -45,6 +50,23 @@ public class MillBlock extends ContainerBlock {
 
             return ActionResultType.CONSUME;
         }
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        Direction enumFacing = context.getHorizontalDirection().getOpposite();
+        return super.getStateForPlacement(context).setValue(FACING, enumFacing);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
     }
 
     @Override
@@ -69,5 +91,10 @@ public class MillBlock extends ContainerBlock {
     @Override
     public BlockRenderType getRenderShape(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
